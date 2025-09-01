621cbb6532245d20d253db1d5085695969580752
Aj Romblon <111903577+Yongbi21@users.noreply.github.com>
2025-09-02 07:12:41 +0800

    feat:login-screen-revised-ui

---
+++ app/src/main/java/com/certicode/inventiapp/fragment/SignUpFragment.kt
@@ -0,0 +1,32 @@
+package com.certicode.inventiapp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.certicode.inventiapp.databinding.FragmentSignupBinding
+
+class SignUpFragment: Fragment() {
+
+
+    private var _binding: FragmentSignupBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
