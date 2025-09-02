43a23fedb5f36476bfd818f1fd3efc83ba22e7cc
Aj Romblon <111903577+Yongbi21@users.noreply.github.com>
2025-09-02 09:48:49 +0800

    feat:signup-screen

--- app/src/main/java/com/certicode/inventiapp/fragment/SignUpFragment.kt
+++ app/src/main/java/com/certicode/inventiapp/fragment/SignUpFragment.kt
@@ -1,10 +1,14 @@
 package com.certicode.inventiapp.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.certicode.inventiapp.activity.HomeActivity
+import com.certicode.inventiapp.activity.MainActivity
+import com.certicode.inventiapp.databinding.ActivityMainBinding
 import com.certicode.inventiapp.databinding.FragmentSignupBinding
 
 class SignUpFragment: Fragment() {
@@ -23,6 +27,17 @@ class SignUpFragment: Fragment() {
 
     }
 
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Sign Up button click listener
+        binding.signInText.setOnClickListener {
+            // Navigate to HomeActivity after sign up
+            val intent = Intent(requireActivity(), MainActivity::class.java)
+            startActivity(intent)
+        }
+  }
 
     override fun onDestroyView() {
         super.onDestroyView()
