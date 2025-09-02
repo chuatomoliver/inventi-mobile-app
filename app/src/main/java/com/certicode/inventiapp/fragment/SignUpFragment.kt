9eaee55eeb91ead1da09aa65695f58c451db6369
Aj Romblon <111903577+Yongbi21@users.noreply.github.com>
2025-09-02 18:32:20 +0800

    fix:changes

--- app/src/main/java/com/certicode/inventiapp/fragment/SignUpFragment.kt
+++ app/src/main/java/com/certicode/inventiapp/fragment/SignUpFragment.kt
@@ -1,10 +1,15 @@
 package com.certicode.inventiapp.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.certicode.inventiapp.R
+import com.certicode.inventiapp.activity.HomeActivity
+import com.certicode.inventiapp.activity.MainActivity
+import com.certicode.inventiapp.databinding.ActivityMainBinding
 import com.certicode.inventiapp.databinding.FragmentSignupBinding
 
 class SignUpFragment: Fragment() {
@@ -23,6 +28,26 @@ class SignUpFragment: Fragment() {
 
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Sign Up button click listener
+        binding.signInText.setOnClickListener {
+            // Navigate to HomeActivity after sign up
+            val intent = Intent(requireActivity(), MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.signUpButton.setOnClickListener {
+            val fragment = OtpFragment() // Replace with your fragment class
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment) // Your container ID in activity layout
+                .addToBackStack(null) // optional: allows back navigation
+                .commit()
+        }
+
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
