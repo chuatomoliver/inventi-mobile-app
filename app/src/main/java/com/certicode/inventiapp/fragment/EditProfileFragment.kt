59d5f3b5c9a088354a97b9b3765811aaa9bda2a0
Tom Oliver Chua <Tom.Chua@ibm.com>
2025-09-03 12:50:16 +0800

    fix fragment_edit_profile

--- app/src/main/java/com/certicode/inventiapp/fragment/EditProfileFragment.kt
+++ app/src/main/java/com/certicode/inventiapp/fragment/EditProfileFragment.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.certicode.inventiapp.R
+
+import com.certicode.inventiapp.databinding.FragmentEditProfileBinding
 
 class EditProfileFragment : Fragment() {
+
+    private var _binding: FragmentEditProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        _binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
 
