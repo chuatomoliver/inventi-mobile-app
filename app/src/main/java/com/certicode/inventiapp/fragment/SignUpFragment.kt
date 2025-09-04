982bb29fce27b68b8b3473695bdafd0686618998
Aaron Mercado <mercadoaaron_bsit@plmun.edu.ph>
2025-09-04 11:39:16 +0800

    fix bug in activity main

--- app/src/main/java/com/certicode/inventiapp/fragment/SignUpFragment.kt
+++ app/src/main/java/com/certicode/inventiapp/fragment/SignUpFragment.kt
@@ -6,17 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-<<<<<<< HEAD
-=======
 import com.certicode.inventiapp.R
->>>>>>> main
-import com.certicode.inventiapp.activity.HomeActivity
 import com.certicode.inventiapp.activity.MainActivity
-import com.certicode.inventiapp.databinding.ActivityMainBinding
 import com.certicode.inventiapp.databinding.FragmentSignupBinding
 
 class SignUpFragment: Fragment() {
-
 
     private var _binding: FragmentSignupBinding? = null
     private val binding get() = _binding!!
@@ -26,46 +20,29 @@ class SignUpFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSignupBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
-
+        return binding.root
     }
 
-
-<<<<<<< HEAD
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-=======
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
->>>>>>> main
         super.onViewCreated(view, savedInstanceState)
 
         // Sign Up button click listener
         binding.signInText.setOnClickListener {
-            // Navigate to HomeActivity after sign up
             val intent = Intent(requireActivity(), MainActivity::class.java)
             startActivity(intent)
         }
 
         binding.signUpButton.setOnClickListener {
-<<<<<<< HEAD
-            val intent = Intent(requireActivity(), MainActivity::class.java)
-            startActivity(intent)
-        }
-  }
-=======
-            val fragment = OtpFragment() // Replace with your fragment class
+            val fragment = OtpFragment() // Assuming OtpFragment is a valid class
             parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, fragment) // Your container ID in activity layout
-                .addToBackStack(null) // optional: allows back navigation
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
                 .commit()
         }
-
     }
->>>>>>> main
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
