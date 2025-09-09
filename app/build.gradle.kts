50aad711d522ae91c532290f822cffbd43e8f262
Aaron Mercado <mercadoaaron_bsit@plmun.edu.ph>
2025-09-09 23:14:06 +0800

    Bring font, color, and array from favorite-fragment-branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-parcelize")
 }
 
 android {
@@ -50,6 +51,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -67,5 +69,14 @@ dependencies {
 
     //Google Maps
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+
+    //Splash Screen
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    
+
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
 
 }
